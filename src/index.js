b5b597b36ad8a449f81b9fb000228e1e73810ff2
Caique Caleiro <caiquecaleiro@gmail.com>
2016-11-23 22:03:29 -0200

    Add initial project structure

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <h1>React Blog!!!</h1>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
+
