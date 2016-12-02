b46406787b26d54e855f7bdd2d740f5a4aaca86c
Caique Caleiro <caiquecaleiro@gmail.com>
2016-12-02 20:33:54 -0200

    Add assets folder with new images for github page. Also, fix the margin top for all the pages

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -20,34 +20,32 @@ class PostsNew extends Component {
     const { fields: { title, categories, content }, handleSubmit } = this.props;
 
     return (
-      <div className="margin-top-25px">
-        <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
-          <h3>Create a new post</h3>
-          <div className={`form-group ${title.touched && title.invalid ? 'invalid' : ''}`}>
-            <label>Title</label>
-            <input type="text" className="form-control" {...title} />
-            <div className="text-help form-control-label">
-              {title.touched ? title.error : ''}
-            </div>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <h3>Create a new post</h3>
+        <div className={`form-group ${title.touched && title.invalid ? 'invalid' : ''}`}>
+          <label>Title</label>
+          <input type="text" className="form-control" {...title} />
+          <div className="text-help form-control-label">
+            {title.touched ? title.error : ''}
           </div>
-          <div className={`form-group ${categories.touched && categories.invalid ? 'invalid' : ''}`}>
-            <label>Categories</label>
-            <input type="text" className="form-control" {...categories}/>
-            <div className="text-help form-control-label">
-              {categories.touched ? categories.error : ''}
-            </div>
+        </div>
+        <div className={`form-group ${categories.touched && categories.invalid ? 'invalid' : ''}`}>
+          <label>Categories</label>
+          <input type="text" className="form-control" {...categories}/>
+          <div className="text-help form-control-label">
+            {categories.touched ? categories.error : ''}
           </div>
-          <div className={`form-group ${content.touched && content.invalid ? 'invalid' : ''}`}>
-            <label>Content</label>
-            <textarea type="text" className="form-control" {...content}/>
-            <div className="text-help form-control-label">
-              {content.touched ? content.error : ''}
-            </div>
+        </div>
+        <div className={`form-group ${content.touched && content.invalid ? 'invalid' : ''}`}>
+          <label>Content</label>
+          <textarea type="text" className="form-control" {...content}/>
+          <div className="text-help form-control-label">
+            {content.touched ? content.error : ''}
           </div>
-          <button type="submit" className="btn btn-primary">Save</button>
-          <Link to="/" className="btn btn-danger margin-left-5px">Cancel</Link>
+        </div>
+        <button type="submit" className="btn btn-primary">Save</button>
+        <Link to="/" className="btn btn-danger margin-left-5px">Cancel</Link>
         </form>
-      </div>
     );
   }
 }
